26e1b7b7fa7eb0ecb9c724019aa10c59868faddf
BlackBloodSwan <domenicschalla@gmail.com>
2021-04-07 18:07:00 +0200

    Interface

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -11,20 +11,17 @@ ReactDOM.render(
     <Header>
       <div className="logo" />
       <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
-        <Menu.Item key="1">nav 1</Menu.Item>
-        <Menu.Item key="2">nav 2</Menu.Item>
-        <Menu.Item key="3">nav 3</Menu.Item>
+        <Menu.Item key="1">Overview</Menu.Item>
+        <Menu.Item key="2">Voice Bot</Menu.Item>
+        <Menu.Item key="3">Chatbot</Menu.Item>
       </Menu>
     </Header>
     <Content style={{ padding: '0 50px' }}>
       <Breadcrumb style={{ margin: '16px 0' }}>
-        <Breadcrumb.Item>Home</Breadcrumb.Item>
-        <Breadcrumb.Item>List</Breadcrumb.Item>
-        <Breadcrumb.Item>App</Breadcrumb.Item>
       </Breadcrumb>
       <div className="site-layout-content">Content</div>
     </Content>
-    <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+    <Footer style={{ textAlign: 'center' }}>Hi</Footer>
   </Layout>,
   document.getElementById('container'),
 );
