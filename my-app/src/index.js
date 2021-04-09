323f6920b3d6ca41b03666d6dbaa93f8022ce1ce
BlackBloodSwan <domenicschalla@gmail.com>
2021-04-09 16:50:15 +0200

    p

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,25 +3,44 @@ import ReactDOM from 'react-dom';
 import 'antd/dist/antd.css';
 import './index.css';
 import { Layout, Menu, Breadcrumb } from 'antd';
+import { Input, Space } from 'antd';
+import { AudioOutlined } from '@ant-design/icons';
+
 
 const { Header, Content, Footer } = Layout;
+const suffix = (
+  <AudioOutlined
+    style={{
+      fontSize: 16,
+      color: '#1890ff',
+    }}
+  />
+);
 
+const handleChatbot = (event) => {
+  window.open("Chatbot.js")
+}
+const handleOverview = (event) => {
+  alert("Hallo")
+}
+const handleVoicebot = (event) => {
+  alert("Hallo")
+}
 ReactDOM.render(
   <Layout className="layout">
     <Header>
       <div className="logo" />
       <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
-        <Menu.Item key="1">Overview</Menu.Item>
-        <Menu.Item key="2">Voice Bot</Menu.Item>
-        <Menu.Item key="3">Chatbot</Menu.Item>
+        <Menu.Item key="1" onClick={handleOverview}>Overview</Menu.Item>
+        <Menu.Item key="2" onClick={handleVoicebot}>Voice Bot</Menu.Item>
+        <Menu.Item key="3" onClick={handleChatbot}>Chatbot</Menu.Item>
       </Menu>
     </Header>
     <Content style={{ padding: '0 50px' }}>
       <Breadcrumb style={{ margin: '16px 0' }}>
       </Breadcrumb>
-      <div className="site-layout-content">Content</div>
     </Content>
-    <Footer style={{ textAlign: 'center' }}>Hi</Footer>
+    <Footer style={{ textAlign: 'center' }}>Made with love by Domenic</Footer>
   </Layout>,
   document.getElementById('container'),
 );
